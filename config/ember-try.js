8b4d5df8de2f9b99f9c82b08b63585d5e963cbdb
Manuel Wiedenmann <manuel@funkensturm.de>
2021-10-09 19:27:22 +0200

    Fix ember-try

--- config/ember-try.js
+++ config/ember-try.js
@@ -22,60 +22,61 @@ module.exports = async function () {
             'ember-source': '~3.24.3',
           },
         },
-        {
-          name: 'ember-release',
-          npm: {
-            devDependencies: {
-              'ember-source': await getChannelURL('release'),
-            },
+      },
+      {
+        name: 'ember-release',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('release'),
           },
         },
-        {
-          name: 'ember-beta',
-          npm: {
-            devDependencies: {
-              'ember-source': await getChannelURL('beta'),
-            },
+      },
+      {
+        name: 'ember-beta',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('beta'),
           },
         },
-        {
-          name: 'ember-canary',
-          npm: {
-            devDependencies: {
-              'ember-source': await getChannelURL('canary'),
-            },
+      },
+      {
+        name: 'ember-canary',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('canary'),
           },
         },
-        {
-          name: 'ember-default-with-jquery',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({
-              'jquery-integration': true,
-            }),
-          },
-          npm: {
-            devDependencies: {
-              '@ember/jquery': '^1.1.0',
-            },
+      },
+      {
+        name: 'ember-default-with-jquery',
+        env: {
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({
+            'jquery-integration': true,
+          }),
+        },
+        npm: {
+          devDependencies: {
+            '@ember/jquery': '^1.1.0',
           },
         },
-        {
-          name: 'ember-classic',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({
-              'application-template-wrapper': true,
-              'default-async-observers': false,
-              'template-only-glimmer-components': false,
-            }),
-          },
-          npm: {
-            ember: {
-              edition: 'classic',
-            },
+      },
+      {
+        name: 'ember-classic',
+        env: {
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({
+            'application-template-wrapper': true,
+            'default-async-observers': false,
+            'template-only-glimmer-components': false,
+          }),
+        },
+        npm: {
+          ember: {
+            edition: 'classic',
           },
         },
+      },
       embroiderSafe(),
       embroiderOptimized(),
     ],
   };
-})};
+};
